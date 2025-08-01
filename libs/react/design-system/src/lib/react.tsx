45f9a85b1cdebc4e20dff8e0226d4c80862517b1
shanxn <dharshanm22@gmail.com>
2025-08-01 15:38:09 +0530

    Feat(React): add storybook integration of react

---
+++ libs/react/design-system/src/lib/react.tsx
@@ -0,0 +1,12 @@
+import BoxComponent from "./BoxComponents/box";
+
+export function React() {
+  return (
+    <div>
+      <h1>Welcome to React!</h1>
+      <BoxComponent />
+    </div>
+  );
+}
+
+export default React;
